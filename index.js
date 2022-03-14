c4cbd92f379c8c81ff21d6ffe3a71677277cdce3
Gokul S <gokul270520@gmail.com>
2022-03-14 17:35:07 +0530

    added another npm

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 
 
 const superheroes = require('superheroes');
- 
+const supervillains = require('supervillains');
 superheroes.all;
+ 
+supervillains.all;
+let result1=superheroes.random();
+let result2=supervillains.random();
 
-let result=superheroes.random();
+console.log(result1);
+console.log(result2);
 
-console.log(result);
+
